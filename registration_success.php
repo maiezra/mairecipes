a85d7790c2a5cae7f92b02101a93b3110459f92b
maiiiiiiiiiiiiiiiiiiiiii <maiez@mta.ac.il>
2024-07-06 11:47:59 +0300

    Delete registration_success.php

--- registration_success.php
+++
@@ -1,8 +0,0 @@
-<?php
-include __DIR__ . '/includes/header.php';
-?>
-
-<h2>Registration Successful</h2>
-<p>You have successfully registered. You can now <a href="login.php">login</a>.</p>
-
-<?php include __DIR__ . '/includes/footer.php'; ?>
